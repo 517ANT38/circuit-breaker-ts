9dccc282eead60680bab5a7ff77993bd22b87cd6
anton <a.tashchilin@bk.ru>
2024-03-09 15:45:32 +0400

    add chnage on express

--- server/server.ts
+++ server/server.ts
@@ -1,52 +1,35 @@
-import http  from "http"
-
+import express, { Response }  from "express"
+const app = express();
 const DELAY = 10;
 const ENDPOINT = "/app";
 const ERRS = [500,502,503,504,508]
 let errorOrSuccess = Math.random();
 
-const halding = (res:any,s="Success req")=>{
+
+const halding = (res:Response<any, any>,s="Success req")=>{
     if (errorOrSuccess < 0.5) {
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        res.end(JSON.stringify({msg:s}));
-        errorOrSuccess = Math.random();
+        res.status(200).json({msg:s});
     } else {
-        res.statusCode = ERRS[Math.floor(Math.random()*(ERRS.length))];
-        res.setHeader("Content-Type", "application/json");
-        res.end(`{"msg":"Error with status code ${res.statusCode}"}`);
+        let index = Math.floor(Math.random()*(ERRS.length));
+        res.status(ERRS[index]);        
+        res.json({msg:`Error with status code ${res.statusCode}`});
         errorOrSuccess = Math.random();
     }
 }
-const server = http.createServer((req, res) => {
-    if (req.url == ENDPOINT) {    
-        if (req.method=="GET") {
-            halding(res);
-        }
-        else {
-            let resBody:any = [];
-            req.on("data",(chunk) =>{
-                resBody.push(chunk);
-            });
-            req.on("end",()=>{
-                try {
-                    resBody = JSON.parse(Buffer.concat(resBody).toString());
-                } catch(e) {
-                    console.log(e);
-                }
-                halding(res,resBody.data);
-            });
-        }
-    }
-  
+
+app.get(ENDPOINT,(req,res)=>{
+    halding(res);
 });
+app.post(ENDPOINT,(req,res)=>{
+    halding(res,req.body)
+})
 export default function startServer(host="localhost",port=9797){
     setTimeout(function change(){
         errorOrSuccess = Math.abs(errorOrSuccess - 1);
         setTimeout(change,Math.random()*DELAY)
     },Math.random()*DELAY);
 
-    server.listen(port,host, () => {
+    app.listen(port,host, () => {
         console.log(`Server is running on http://${host}:${port}${ENDPOINT}`);
     });
 }
