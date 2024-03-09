1ea42f5200e46d3f0445d45603ac55aab073d911
anton <a.tashchilin@bk.ru>
2024-03-09 08:37:07 +0400

    add change index

--- index.ts
+++ index.ts
@@ -4,13 +4,15 @@ import startServer from "./server/server";
 const URL = "http://localhost:9797/app";
 startServer();
 
-const axios = new WrapperAxios();
+const axios = new WrapperAxios({
+    validateStatus: (status:number) =>true 
+});
 const cb = new CircuitBreaker(axios);
 (async ()=>{
     for (let index = 0; index < 100; index++) {
         try{
             let res = await cb.request(URL);
-            console.log(res)
+            console.log("Server responce",res.status)
         }catch(e){
             console.log(e)
         }
